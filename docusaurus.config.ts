fdef18ee49c061b4698f97eabc83c5768cfbe5a9
suzuki.masato <suzuki.masato@a-tm.co.jp>
2024-09-11 11:55:08 +0900

    Revert "tutorial: 🎓 Done manage docs versions (#6)"
    This reverts commit 4371809e2a3ea44d98e85c05f9ee411cd14e10c4.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -84,9 +84,6 @@ const config: Config = {
         },
         {to: '/blog', label: 'Blog', position: 'left'},
         {
-          type: 'docsVersionDropdown',
-        },
-        {
           href: 'https://github.com/masato1224/my-scrap-note',
           label: 'GitHub',
           position: 'right',
